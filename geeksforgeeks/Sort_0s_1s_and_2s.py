1f43f8e8314ad17872aec2340bf32c95661ffabf
AkhilKompally <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-05 14:46:05 +0530

    Add solution: Sort 0s, 1s and 2s

---
+++ geeksforgeeks/Sort_0s_1s_and_2s.py
@@ -0,0 +1,11 @@
+# URL: https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+#
+# Problem: Sort 0s, 1s and 2s
+#
+# Time Complexity: O(n)Auxiliary Space: O(1)
+
+# Solution:
+class Solution:
+    def sort012(self, arr):
+        # code here
+        arr.sort()
