1ec1137d3b35352a9824a9915180a41c4896bf15
AkhilKompally <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-05 14:45:43 +0530

    Add solution: Sort 0s, 1s and 2s

---
+++ geeksforgeeks/Sort_0s_1s_and_2s.java
@@ -0,0 +1,13 @@
+// URL: https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+//
+// Problem: Sort 0s, 1s and 2s
+//
+// Time Complexity: O(n)Auxiliary Space: O(1)
+
+// Solution:
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
