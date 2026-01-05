ed4ffa7abc1d20f5981a76a27061e1eee20b65ab
AkhilKompally <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-05 18:51:51 +0530

    Add solution: Count elements less than or equal to k in a sorted rotated array

---
+++ geeksforgeeks/Count_elements_less_than_or_equal_to_k_in_a_sorted_rotated_array.py
@@ -0,0 +1,10 @@
+# URL: https://www.geeksforgeeks.org/problems/count-elements-less-than-or-equal-to-k-in-a-sorted-rotated-array/1
+#
+# Problem: Count elements less than or equal to k in a sorted rotated array
+#
+# Time Complexity: O(log n)Auxiliary Space: O(1)
+
+# Solution:
+class Solution:
+    def countLessEqual(self, arr, x):
+        #code here
