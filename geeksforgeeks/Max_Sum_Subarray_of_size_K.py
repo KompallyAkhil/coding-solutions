36972613b0c020bba8d0d9f429ea03faec393a3f
AkhilKompally <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-05 14:38:53 +0530

    Add solution: Max Sum Subarray of size K

--- geeksforgeeks/Max_Sum_Subarray_of_size_K.py
+++ geeksforgeeks/Max_Sum_Subarray_of_size_K.py
@@ -8,6 +8,10 @@
 class Solution {
     public int maxSubarraySum(int[] arr, int k) {
         // Code here
-
+        cont_sum = res = sum(arr[:k])
+        for i in range(k,len(arr)):
+            cont_sum = cont_sum - arr[i-k] + arr[i]
+            res = max(cont_sum,res)
+        return res
     }
 }
