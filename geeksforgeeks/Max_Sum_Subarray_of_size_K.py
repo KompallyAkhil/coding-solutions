a1db5edf43d4a43fea6342555e115742306ca641
AkhilKompally <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-05 14:38:00 +0530

    Add solution: Max Sum Subarray of size K

---
+++ geeksforgeeks/Max_Sum_Subarray_of_size_K.py
@@ -0,0 +1,13 @@
+# URL: https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+#
+# Problem: Max Sum Subarray of size K
+#
+# Time Complexity: O(n)Auxiliary Space: O(1)
+
+# Solution:
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+
+    }
+}
