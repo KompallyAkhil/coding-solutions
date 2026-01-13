a76f0abba39350485eafe5013a4bb75878c06402
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-13 14:40:58 +0530

    Add solution: 3453. Separate Squares I

---
+++ leetcode/3453_Separate_Squares_I.java
@@ -0,0 +1,11 @@
+// URL: https://leetcode.com/problems/separate-squares-i/description/?envType=daily-question&envId=2026-01-13
+//
+// Problem: 3453. Separate Squares I
+//
+
+// Solution:
+class Solution {
+    public double separateSquares(int[][] squares) {
+        
+    }
+}
