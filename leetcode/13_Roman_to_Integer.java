a7847d0c33a362c3138bff461ae0fd6a6ffe44e9
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-13 11:38:34 +0530

    Add solution: 13. Roman to Integer

--- leetcode/13_Roman_to_Integer.java
+++ leetcode/13_Roman_to_Integer.java
@@ -4,24 +4,31 @@
 //
 
 // Solution:
-values.put("C", 100);
-        values.put("D", 500);
-        values.put("M", 1000);
-
-        s = s.replace("IV", "IIII").replace("IX", "VIIII");
-        s = s.replace("XL", "XXXX").replace("XC", "LXXXX");
-        s = s.replace("CD", "CCCC").replace("CM", "DCCCC");
-
-        int count = 0;
-        for (Map.Entry<String, Integer> entry : values.entrySet()) {
-            String k = entry.getKey();
-            int v = entry.getValue();
-            for (char i : s.toCharArray()) {
-                if (k.charAt(0) == i) {
-                    count += v;
-                }
-            }
-        }
-        return count;
-    }
+class Solution {
+    public int romanToInt(String s) {
+        Map<String, Integer> values = new HashMap<>();
+        values.put("I", 1);
+        values.put("V", 5);
+        values.put("X", 10);
+        values.put("L", 50);
+        values.put("C", 100);
+        values.put("D", 500);
+        values.put("M", 1000);
+        
+        s = s.replace("IV", "IIII").replace("IX", "VIIII");
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX");
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC");
+        
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : values.entrySet()) {
+            String k = entry.getKey();
+            int v = entry.getValue();
+            for (char i : s.toCharArray()) {
+                if (k.charAt(0) == i) {
+                    count += v;
+                }
+            }
+        }
+        return count;
+    }
 }
