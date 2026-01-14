993144f3f7f7fb02508dc362b83bb3f313634491
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-14 10:44:33 +0530

    Add solution: Police and Thieves

---
+++ geeksforgeeks/Police_and_Thieves.py
@@ -0,0 +1,10 @@
+# URL: https://www.geeksforgeeks.org/problems/police-and-thieves--141631/1
+#
+# Problem: Police and Thieves
+#
+# Time Complexity: O(n)Auxiliary Space: O(1)
+
+# Solution:
+class Solution:
+    def catchThieves(self, arr, k):
+        #code here
