12742b630b6955a3519b4f15a85e484269836984
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-15 15:45:23 +0530

    Add solution: 41. First Missing Positive

---
+++ leetcode/41_First_Missing_Positive.py
@@ -0,0 +1,15 @@
+# URL: https://leetcode.com/problems/first-missing-positive/description/
+#
+# Problem: 41. First Missing Positive
+#
+
+# Solution:
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        dic = {}
+        for i in nums:
+            dic[i] = i
+        for i in range (1,len(nums)+2):
+            if i not in dic:
+                return i
+                break
