d3e33270aefe76520226fae02ce4e9c51ba9e658
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-18 13:20:22 +0530

    Add solution: 1895. Largest Magic Square

---
+++ leetcode/1895_Largest_Magic_Square.java
@@ -0,0 +1,11 @@
+// URL: https://leetcode.com/problems/largest-magic-square/?envType=daily-question&envId=2026-01-18
+//
+// Problem: 1895. Largest Magic Square
+//
+
+// Solution:
+class Solution {
+    public int largestMagicSquare(int[][] grid) {
+        
+    }
+}
