c8c4ff95fcc6d7d848d37880c1a3c3914fd49896
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-19 21:08:23 +0530

    Add solution: 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold

---
+++ leetcode/1292_Maximum_Side_Length_of_a_Square_with_Sum_Less_than_or_Equal_to_Threshold.java
@@ -0,0 +1,11 @@
+// URL: https://leetcode.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/description/?envType=daily-question&envId=2026-01-19
+//
+// Problem: 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+//
+
+// Solution:
+class Solution {
+    public int maxSideLength(int[][] mat, int threshold) {
+        
+    }
+}
