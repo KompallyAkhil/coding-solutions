789bb83f92432b014d58e581b465dd3b894e41ca
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-21 21:24:21 +0530

    Add solution: 3315. Construct the Minimum Bitwise Array II

---
+++ leetcode/3315_Construct_the_Minimum_Bitwise_Array_II.py
@@ -0,0 +1,9 @@
+# URL: https://leetcode.com/problems/construct-the-minimum-bitwise-array-ii/description/?envType=daily-question&envId=2026-01-21
+#
+# Problem: 3315. Construct the Minimum Bitwise Array II
+#
+
+# Solution:
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
