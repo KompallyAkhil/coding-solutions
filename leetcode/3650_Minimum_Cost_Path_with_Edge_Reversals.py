b4aae955982ede7a81bfc940af30eb98602cbef0
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-27 20:40:21 +0530

    Add solution: 3650. Minimum Cost Path with Edge Reversals

---
+++ leetcode/3650_Minimum_Cost_Path_with_Edge_Reversals.py
@@ -0,0 +1,8 @@
+# URL: https://leetcode.com/problems/minimum-cost-path-with-edge-reversals/description/?envType=daily-question&envId=2026-01-27
+#
+# Problem: 3650. Minimum Cost Path with Edge Reversals
+#
+
+# Solution:
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]]) -> int:
