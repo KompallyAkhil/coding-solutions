82010b673b0ca9c4f7772c6a5a00ae412bdb98a0
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-01-27 20:40:53 +0530

    Add solution: 3650. Minimum Cost Path with Edge Reversals

--- leetcode/3650_Minimum_Cost_Path_with_Edge_Reversals.py
+++ leetcode/3650_Minimum_Cost_Path_with_Edge_Reversals.py
@@ -1,4 +1,4 @@
-# URL: https://leetcode.com/problems/minimum-cost-path-with-edge-reversals/description/?envType=daily-question&envId=2026-01-27
+# URL: https://leetcode.com/problems/minimum-cost-path-with-edge-reversals/?envType=daily-question&envId=2026-01-27
 #
 # Problem: 3650. Minimum Cost Path with Edge Reversals
 #
