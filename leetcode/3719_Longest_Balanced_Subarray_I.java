04bfe9eae799f31678ee7afedb2afbf4171c45ef
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-02-10 15:05:00 +0530

    Add solution: 3719. Longest Balanced Subarray I

--- leetcode/3719_Longest_Balanced_Subarray_I.java
+++ leetcode/3719_Longest_Balanced_Subarray_I.java
@@ -1,29 +1,24 @@
-// URL: https://leetcode.com/problems/longest-balanced-subarray-i/submissions/?envType=daily-question&envId=2026-02-10
+// URL: https://leetcode.com/problems/longest-balanced-subarray-i/submissions/1914479833/?envType=daily-question&envId=2026-02-10
 //
 // Problem: 3719. Longest Balanced Subarray I
 //
 
 // Solution:
 class Solution {
-    public static int longestBalance(int [] nums , int start , int end){
-        Set<Integer> evenSet = new HashSet<>();
-        Set<Integer> oddSet = new HashSet<>();
-
-        for (int i = start; i <= end; i++) {
-            if (nums[i] % 2 == 0) {
-                evenSet.add(nums[i]);
-            } else {
-                oddSet.add(nums[i]);
-            }
-        }
-
-        return evenSet.size() == oddSet.size() ? end - start + 1 : -1;
-    }
     public int longestBalanced(int[] nums) {
         int maximum = 0;
         for(int i = 0 ; i < nums.length ; i++){
+            Set<Integer> evenSet = new HashSet<>();
+            Set<Integer> oddSet = new HashSet<>();
             for (int j = i ; j < nums.length ; j++){
-                maximum = Math.max(maximum,longestBalance(nums,i,j));
+                if (nums[j] % 2 == 0)
+                    evenSet.add(nums[j]);
+                else
+                    oddSet.add(nums[j]);
+
+                if (evenSet.size() == oddSet.size()) {
+                    maximum = Math.max(maximum,j - i + 1);
+                }
             }
         }
         return maximum;
