af5ffcabc3c0b8151dcc9d4c5f8a105fc59383e5
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-02-16 16:43:25 +0530

    Add solution: 190. Reverse Bits

---
+++ leetcode/190_Reverse_Bits.java
@@ -0,0 +1,12 @@
+// URL: https://leetcode.com/problems/reverse-bits/submissions/1920963537/?envType=daily-question&envId=2026-02-16
+//
+// Problem: 190. Reverse Bits
+//
+
+// Solution:
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
