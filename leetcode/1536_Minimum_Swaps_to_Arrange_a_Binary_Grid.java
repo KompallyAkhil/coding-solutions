aad7e549e24e8b2bdf3a0bb20854e51a2ba2237d
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-03-02 21:15:26 +0530

    Add solution: 1536. Minimum Swaps to Arrange a Binary Grid

---
+++ leetcode/1536_Minimum_Swaps_to_Arrange_a_Binary_Grid.java
@@ -0,0 +1,11 @@
+// URL: https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/description/?envType=daily-question&envId=2026-03-02
+//
+// Problem: 1536. Minimum Swaps to Arrange a Binary Grid
+//
+
+// Solution:
+class Solution {
+    public int minSwaps(int[][] grid) {
+        
+    }
+}
