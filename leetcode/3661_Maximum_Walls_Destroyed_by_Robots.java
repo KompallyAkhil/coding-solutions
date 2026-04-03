edf6a7ff0404ed3a0a0f9ec50748e62f9399c90c
KompallyAkhil <156333073+KompallyAkhil@users.noreply.github.com>
2026-04-03 11:41:41 +0530

    Add solution: 3661. Maximum Walls Destroyed by Robots

---
+++ leetcode/3661_Maximum_Walls_Destroyed_by_Robots.java
@@ -0,0 +1,11 @@
+// URL: https://leetcode.com/problems/maximum-walls-destroyed-by-robots/description/?envType=daily-question&envId=2026-04-03
+//
+// Problem: 3661. Maximum Walls Destroyed by Robots
+//
+
+// Solution:
+class Solution {
+    public int maxWalls(int[] robots, int[] distance, int[] walls) {
+        
+    }
+}
